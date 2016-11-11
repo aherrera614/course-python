56bbea861a43bd4cc3ca8b399cd33373b4432137
Ariel Herrera <arielherrera@Ariels-Air.home>
2016-11-11 09:19:33 -0500

    first commit

---
+++ tkinter/script1.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+window=Tk()
+
+def km_to_miles():
+    print("success")
+
+b1=Button(window,text="Execute",command=km_to_miles)
+b1.grid(row=0,column=0,rowspan=2)
+
+e1=Entry(window)
+e1.grid(row=0,column=1)
+
+t1=Text(window,height=1,width=20)
+t1.grid(row=0,column=2)
+
+window.mainloop()
